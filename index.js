c91f46d75041d04b1917acff788b2ad4b6da7760
Tomer Revah <revahtomer@gmail.com>
2023-01-15 19:49:31 +0200

    fix for prod

--- index.js
+++ index.js
@@ -12,11 +12,7 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.static('public'));
-
-if (process.env.NODE_ENV === 'production') {
-  // Express serve static files on production environment
-  app.use(express.static(path.resolve(__dirname, 'public')));
-}
+app.use(express.static(path.resolve(__dirname, 'public')));
 
 let activePlayers = 0;
 let roleCounter = true;
