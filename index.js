2aae76e507d5b580a500ac07eb18648cf45bfba6
Tomer Revah <revahtomer@gmail.com>
2023-01-15 19:32:54 +0200

    build ssr

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const io = require('socket.io')(http, {
   cors: { origin: '*' },
 });
 
-app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.static('public'));
+
+if (process.env.NODE_ENV === 'production') {
+  // Express serve static files on production environment
+  app.use(express.static(path.resolve(__dirname, 'public')));
+}
 
 let activePlayers = 0;
 let roleCounter = true;
@@ -17,7 +23,6 @@ let roleCounter = true;
 app.use('/words', require('./routes/words'));
 
 io.on('connection', (socket) => {
-  console.log('tomer', socket);
   roleCounter ? (roleCounter = false) : (roleCounter = true);
   socket.on('mode picked', () => {
     socket.broadcast.emit('mode picked');
@@ -65,58 +70,5 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3001;
-
-app.get('/', (req, res) => res.type('html').send(html));
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`;
 
 http.listen(PORT, () => console.log('Running on port ', PORT));
