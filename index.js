216cb66000ffeac18622ca0111e01e84c9fa8680
Tomer Revah <revahtomer@gmail.com>
2023-01-15 18:33:26 +0200

    clear

--- index.js
+++ index.js
@@ -60,10 +60,6 @@ io.on('connection', (socket) => {
   });
 });
 
-app.get('/**', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 const PORT = process.env.PORT || 4000;
 
 http.listen(PORT, () => console.log('Running on port ', PORT));
